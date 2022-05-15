e2e994475c05526de10bbefe2b5ebef446e151f9
greatstep93 <greatstep93@gmail.com>
2022-05-15 19:25:08 +0400

    Создание бина Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,25 @@
+import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
+
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean2));
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.equals(beanCat2));
+
+
+
+
     }
 }
